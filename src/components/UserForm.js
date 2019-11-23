ca53050a7896c7b41b529110a25a0bd694187294
Seth NeJame <sethnejame@gmail.com>
2019-11-23 13:35:27 +0100

    Adding switch statement that switches component view based on what step we're on

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from "react";
+import FormPersonalDetails from "./FormPersonalDetails";
+import FormUserDetails from "./FormUserDetails";
+import Confirm from "./Confirm";
+import Success from "./Success";
 
 const UserForm = () => {
   const [user, setUser] = useState({
@@ -23,14 +27,24 @@ const UserForm = () => {
 
   const handleChange = e => {
     e.preventDefault();
-    setUser({ ...user, [e.target.name]: e.target.value})
-  }
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
 
-  return (
-    <div>
-      <h1>User Form</h1>
-    </div>
-  );
+  const { step, firstName, lastName, email, occupation, city, bio } = user;
+  const values = { step, firstName, lastName, email, occupation, city, bio };
+
+  switch (step) {
+    case 1:
+      return <FormPersonalDetails values={values} />;
+    case 2:
+      return <FormUserDetails values={values} />;
+    case 3:
+      return <Confirm values={values} />;
+    case 4:
+      return <Success values={values} />;
+    default:
+      return;
+  }
 };
 
 export default UserForm;
