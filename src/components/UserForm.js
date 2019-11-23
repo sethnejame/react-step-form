2e146c738fda15cf756af425dac187455591ae06
Seth NeJame <sethnejame@gmail.com>
2019-11-23 13:13:05 +0100

    Basic state for user

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React { useState, useEffect } from "react";
 
-const UserForm = () => (
+const UserForm = () => {
+  const [user, setUser] = useState({
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    occupation: '',
+    city: '',
+    bio: ''
+
+  })
+  return (
+
   <div>
     <h1>User Form</h1>
   </div>
-);
+)}
 
 export default UserForm;
