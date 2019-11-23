2ac5760b345904ffcfbab8764649fafb0a37d46d
Seth NeJame <sethnejame@gmail.com>
2019-11-23 13:28:06 +0100

    handleChange func

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -16,6 +16,16 @@ const UserForm = () => {
     setUser({ step: step + 1 });
   };
 
+  const prevStep = () => {
+    const { step } = user;
+    step > 0 ? setUser({ step: step - 1 }) : console.log("Already at Step 1");
+  };
+
+  const handleChange = e => {
+    e.preventDefault();
+    setUser({ ...user, [e.target.name]: e.target.value})
+  }
+
   return (
     <div>
       <h1>User Form</h1>
