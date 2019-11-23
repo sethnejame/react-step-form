a6f83dc01ee842a74357436dd520d30b20c39b69
Seth NeJame <sethnejame@gmail.com>
2019-11-23 18:58:00 +0100

    Adding personalDeets comp

--- src/components/FormPersonalDetails.js
+++ src/components/FormPersonalDetails.js
@@ -1,9 +1,74 @@
 import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
 
-const FormPersonalDetails = () => (
-  <div>
-    <h1>Form Personal Details</h1>
-  </div>
-);
+const FormPersonalDetails = props => {
+  const moveForward = e => {
+    e.preventDefault();
+    nextStep();
+  };
+
+  const moveBackward = e => {
+    e.preventDefault();
+    prevStep();
+  };
+
+  const { occupation, bio, city } = props.values;
+  const { handleChange, nextStep, prevStep } = props;
+
+  console.log(props.values);
+
+  return (
+    <MuiThemeProvider>
+      <>
+        <AppBar title="Enter User Details" />
+        <TextField
+          hintText="Enter Your Occupation"
+          floatingLabelText="Occupation"
+          onChange={handleChange}
+          name="occupation"
+          defaultValue={occupation}
+        />
+        <br />
+        <TextField
+          hintText="Enter Your City"
+          floatingLabelText="City"
+          onChange={handleChange}
+          name="city"
+          defaultValue={city}
+        />
+        <br />
+        <TextField
+          hintText="Enter Your Bio"
+          floatingLabelText="Bio"
+          onChange={handleChange}
+          name="bio"
+          defaultValue={bio}
+        />
+        <br />
+        <RaisedButton
+          label="Continue"
+          primary={true}
+          style={styles.button}
+          onClick={moveForward}
+        />
+        <RaisedButton
+          label="Go Back"
+          primary={true}
+          style={styles.button}
+          onClick={moveBackward}
+        />
+      </>
+    </MuiThemeProvider>
+  );
+};
+
+const styles = {
+  button: {
+    margin: 15
+  }
+};
 
 export default FormPersonalDetails;
