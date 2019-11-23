921c1ca616d83d93c8b30bee3e8f0a9fa0ef8d00
Seth NeJame <sethnejame@gmail.com>
2019-11-23 13:24:02 +0100

    NextStep func for moving through flow

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,21 +1,26 @@
-import React { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const UserForm = () => {
   const [user, setUser] = useState({
     step: 1,
-    firstName: '',
-    lastName: '',
-    email: '',
-    occupation: '',
-    city: '',
-    bio: ''
+    firstName: "",
+    lastName: "",
+    email: "",
+    occupation: "",
+    city: "",
+    bio: ""
+  });
 
-  })
+  const nextStep = () => {
+    const { step } = user;
+    setUser({ step: step + 1 });
+  };
+
   return (
-
-  <div>
-    <h1>User Form</h1>
-  </div>
-)}
+    <div>
+      <h1>User Form</h1>
+    </div>
+  );
+};
 
 export default UserForm;
