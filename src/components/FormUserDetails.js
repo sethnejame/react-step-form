47000bdb2b53b56bcfba8e16d859f29048ff1c27
Seth NeJame <sethnejame@gmail.com>
2019-11-23 13:49:01 +0100

    Building out FormUserDeets, adding moveForward & pulling in props

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,9 +1,20 @@
 import React from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
 
-const FormUserDetails = () => (
-  <div>
-    <h1>Form User Details</h1>
-  </div>
-);
+const FormUserDetails = props => {
+  const moveForward = e => {
+    e.preventDefault();
+    props.nextStep();
+  };
+  const { step, firstName, lastName, email, occupation, city, bio } = props;
+  return (
+    <div>
+      <h1>Form User Details</h1>
+    </div>
+  );
+};
 
 export default FormUserDetails;
